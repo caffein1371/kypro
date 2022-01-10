8544798fd1abb4797d53e33cb6201ea17f267f00
cahein <incidents1371@gmail.com>
2022-01-10 14:59:31 +0900

    20210110

---
+++ ABC229/A-FirstGrid.py
@@ -0,0 +1,8 @@
+S1 = input()
+S2 = input()
+if S1[0]=="#" and S1[1]!="#" and S2[0]!="#" and S2[1]=="#":
+  print ("No")
+elif  S1[0]!="#" and S1[1]=="#" and S2[0]=="#" and S2[1]!="#":
+  print ("No")
+else:
+  print ("Yes")
