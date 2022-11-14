67cd7e5f9fb183cef4d0dba6fe4b8beae30892b7
cahein <incidents1371@gmail.com>
2022-11-14 11:24:04 +0900

    20221114

--- temp.py
+++ temp.py
@@ -3,35 +3,43 @@ import io
 import sys
 
 _INPUT = """\
-4
-2 4
-4 3
-4 10
-8 1
-
+0 0
+1 0
+1 1
+0 1
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-from collections import defaultdict,deque
-d1 = defaultdict(list)
 
-N = int(input())
-for i in range(N):
-    A,B = map(int,input().split())
-    d1[A].append(B)
-    d1[B].append(A)
+xy = []
+for i in range(4):
+    x,y= map(int,input().split())
+    xy.append([x,y])
 
-que = deque()
-que.append(1)
-S = {1}
-while que:
-    v = que.popleft()
-    #print(v)
-    for i in d1[v]:
-        #print(i)
-        if not i in S:
-            que.append(i)
-            S.add(i)
-print (max(S))
+import itertools
+import numpy as np
 
+
+def degree(a, b, c):
+
+    # ベクトルを定義
+    vec_a = a - b
+    vec_c = c - b
+
+    # コサインの計算
+    length_vec_a = np.linalg.norm(vec_a)
+    length_vec_c = np.linalg.norm(vec_c)
+    inner_product = np.inner(vec_a, vec_c)
+    cos = inner_product / (length_vec_a * length_vec_c)
+
+    # 角度（ラジアン）の計算
+    rad = np.arccos(cos)
+
+    # 弧度法から度数法（rad ➔ 度）への変換
+    degree = np.rad2deg(rad)
+
+    return degree
+
+for v in itertools.combinations(xy,3):
+    print (V)
