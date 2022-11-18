58361ab87bf41a6d1fa5c31daaa0f4f9d70e7982
cahein <incidents1371@gmail.com>
2022-11-18 11:06:47 +0900

    20221118

--- ABC108/B.py
+++ ABC108/B.py
@@ -3,7 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-2 3 6 6
+0 0 0 1
+
+
 
 
 """
@@ -11,10 +13,12 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 x1,y1,x2,y2 = map(int,input().split())
 import math
-temp = math.sqrt((x1-x2)**2+(y1-y2)**2)
-x3=abs(x2-x1)//math.sin(math.radians(45))
-y3=y1+temp
-x4=x1-temp
-y4=y1
 
-print (int(x3),int(y3),int(x4),int(y4))
+
+x3=x1*math.cos(math.radians(-90))-y1*math.sin(math.radians(-90))+x2-x2*math.cos(math.radians(-90))+y2*math.sin(math.radians(-90))
+y3=x1*math.sin(math.radians(-90))+y1*math.cos(math.radians(-90))+y2-x2*math.sin(math.radians(-90))-y2*math.cos(math.radians(-90))
+x4=x2*math.cos(math.radians(90))-y2*math.sin(math.radians(90))+x1-x1*math.cos(math.radians(90))+y1*math.sin(math.radians(90))
+y4=x2*math.sin(math.radians(90))+y2*math.cos(math.radians(90))+y1-x1*math.sin(math.radians(90))-y1*math.cos(math.radians(90))
+
+
+print(int(x3),int(y3),int(x4),int(y4))
