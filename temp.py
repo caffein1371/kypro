33826865fb56138552ded6d4c519f25ba9c7eeaa
cahein <incidents1371@gmail.com>
2022-11-20 09:57:13 +0900

    20221120

--- temp.py
+++ temp.py
@@ -3,15 +3,29 @@ import io
 import sys
 
 _INPUT = """\
-2 8
-1 1 2
-1 2 1
-3 1 2
-1 1 2
-1 1 2
-1 1 2
-2 1 2
-3 1 2
+10
+1 8 4 15 7 5 7 5 8 0
+20
+2 7 0
+3 7
+3 8
+1 7
+3 3
+2 4 4
+2 4 9
+2 10 5
+1 10
+2 4 2
+1 10
+2 3 1
+2 8 11
+2 3 14
+2 1 9
+3 8
+3 8
+3 1
+2 6 5
+3 7
 
 
 
@@ -19,25 +33,17 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,Q = map(int,input().split())
-from collections import defaultdict
 
-d = defaultdict(list)
+
+N = int(input())
+Alist = list(map(int,input().split()))
+Q = int(input())
 for i in range(Q):
-    T,A,B = map(int,input().split())
-    if T==1:
-        d[A].append(B)
-    if T==2:
-        try:
-            d[A].remove(B)
-        except:
-            pass
-    if T==3:
-        # d[A]=list(set(d[A]))
-        # d[B]=list(set(d[B]))
-        if B in d[A] and A in d[B]:
-            print ("Yes")
-        else:
-            print ("No")
-    #print(d)
+    qlist = list(map(int,input().split()))
+    if qlist[0]==1:
+        Alist = [qlist[1] for i in range(N)]
+    elif qlist[0]==2:
+        Alist[qlist[1]-1]+=qlist[2]
+    elif qlist[0]==3:
+        print (Alist[qlist[1]-1])
 
