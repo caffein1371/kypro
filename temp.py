962c062383c788264a77a5c4272050776b52af33
cahein <incidents1371@gmail.com>
2022-11-26 22:48:05 +0900

    20221126

--- temp.py
+++ temp.py
@@ -3,13 +3,28 @@ import io
 import sys
 
 _INPUT = """\
-3 4
+10 1
+
+
+
+
+
+
+
+
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-X,Y = map(int,input().split())
+A,B = map(int,input().split())
+from decimal import Decimal, ROUND_HALF_UP
+#temp = round((2*B/A)**(-2/3)-1)
+temp = Decimal((2*B/A)**(-2/3)-1).quantize(Decimal('1'), rounding=ROUND_HALF_UP) 
+#print (temp)
 import math
-temp = math.sqrt(X**2+Y**2)
-print(X/temp,Y/temp)
+if math.sqrt(temp+1)!=0:
+    #print ((B*temp)+A/math.sqrt(temp+1))
+    print (Decimal((B*temp)+A/math.sqrt(temp+1)).quantize(Decimal('0.00000001'), rounding=ROUND_HALF_UP))
+else:
+    print (A)
