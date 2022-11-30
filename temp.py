e62b70b6bebc72c460dc8199760dedcfe524723e
cahein <incidents1371@gmail.com>
2022-11-30 22:34:33 +0900

    20221130

--- temp.py
+++ temp.py
@@ -3,27 +3,25 @@ import io
 import sys
 
 _INPUT = """\
-10 1
-
+abaababaab
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-from math import sqrt
+S = input()
+import collections
+#print (S)
+flag = True
 
-
-A, B = map(int, input().split())
-
-L, R = 0, (A + B - 1) // B
-
-while L + 1 < R:
-    M = (L + R) // 2
-    condition_0 = A * A > B * B * M * (M + 1) * (2 * M + 1)
-    condition_1 = A ** 4 + B ** 4 * M * M * (M + 1) * (M + 1) > 2 * A * A * B * B * M * (M + 1) * (2 * M + 1)
-    if condition_0 and condition_1:
-        L = M
-    else:
-        R = M
-
-print(A / sqrt(L + 1) + B * L)
+while flag:
+    S =S[:-1]
+    #print (S)
+    #print (c)
+    temp = len(S)//2
+    if len(S)%2==0:
+        if S[0:temp]==S[temp:len(S)]:
+            flag=False
+    
+#print(S)
+print (len(S))
