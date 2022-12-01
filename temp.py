e731985e9aeca6458308f485b0e21f9634e3ac8f
cahein <incidents1371@gmail.com>
2022-12-01 19:55:12 +0900

    20221201

--- temp.py
+++ temp.py
@@ -3,24 +3,16 @@ import io
 import sys
 
 _INPUT = """\
-4
-1 3 5 2
-2 3 1 4
+2 3
+
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N= int(input())
-Alist = list(map(int,input().split()))
-Blist = list(map(int,input().split()))
+N,M = map(int,input().split())
+import itertools
+a = [i for i in range(1,M+1)]
 
-ans = 0
-ans1 = 0
-for i in range(N):
-    if Alist[i]==Blist[i]:
-        ans+=1
-    if Alist[i] in Blist and Alist[i]!=Blist[i]:
-        ans1+=1
-print (ans)
-print (ans1)
+for v in itertools.combinations(a,N):
+    print (*v)
