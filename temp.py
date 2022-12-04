5412e2167ff6f134940e7765cc32c881fc81eba4
cahein <incidents1371@gmail.com>
2022-12-04 11:27:15 +0900

    20221204

--- temp.py
+++ temp.py
@@ -3,16 +3,17 @@ import io
 import sys
 
 _INPUT = """\
-2 3
+Y
+a
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,M = map(int,input().split())
-import itertools
-a = [i for i in range(1,M+1)]
-
-for v in itertools.combinations(a,N):
-    print (*v)
+S = input()
+T = input()
+if S=="Y":
+    print (T.upper())
+else:
+    print (T)
