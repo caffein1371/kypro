66e2b970a1056855816b3b0b6517e2ece6ab1043
cahein <incidents1371@gmail.com>
2022-12-08 17:36:18 +0900

    20221208

--- temp.py
+++ temp.py
@@ -3,9 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-12
-18
-11
+4 11
 
 
 
@@ -16,34 +14,12 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A = int(input())
-B = int(input())
-C = int(input())
-
-if A>B>C:
-  print (1)
-  print (2)
-  print (3)
-elif A>C>B:
-  print (1)
-  print (3)
-  print (2)
-elif B>A>C:
-  print (2)
-  print (1)
-  print (3)
-elif B>C>A:
-  print (3)
-  print (1)
-  print (2)
-elif C>A>B:
-  print (2)
-  print (3)
-  print (1)
-elif C>B>A:
-  print (3)
-  print (2)
-  print (1)
-
+a,b = map(int,input().split())
+if a+b==15:
+    print ("+")
+elif a*b==15:
+    print ("*")
+else:
+    print ("x")
 
 
