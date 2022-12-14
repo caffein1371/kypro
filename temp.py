0abe39e94df3e261e466bb4f2a737733d47dc020
cahein <incidents1371@gmail.com>
2022-12-14 22:01:59 +0900

    20221214

--- temp.py
+++ temp.py
@@ -14,12 +14,16 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 L1,R1,L2,R2 = map(int,input().split())
 
-if R1<L2 or R2<L1:
+if R1<=L2 or R2<=L1:
     print (0)
 elif L1<=L2 and R1<=R2:
     print (R1-L2)
 elif L2<=L1 and R2<=R1:
     print (R2-L1)
+elif L1<=L2 and R2<=R1:
+    print (R2-L2)
+elif L1<=L2 and R2<=R1:
+    print (R2-L2)
 elif L2<R1:
     print (abs(L2-R1))
 elif R2<L1:
