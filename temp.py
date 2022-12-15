7276a603d8fc491f23730c31b5d64b99dde9ccda
cahein <incidents1371@gmail.com>
2022-12-15 10:30:50 +0900

    20221215

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-0 3 3 7
+32
 
 
 
@@ -12,19 +12,6 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-L1,R1,L2,R2 = map(int,input().split())
-
-if R1<=L2 or R2<=L1:
-    print (0)
-elif L1<=L2 and R1<=R2:
-    print (R1-L2)
-elif L2<=L1 and R2<=R1:
-    print (R2-L1)
-elif L1<=L2 and R2<=R1:
-    print (R2-L2)
-elif L1<=L2 and R2<=R1:
-    print (R2-L2)
-elif L2<R1:
-    print (abs(L2-R1))
-elif R2<L1:
-    print (abs(L1-R2))
+ans = [1, 1, 1, 2, 1, 2, 1, 5, 2, 2, 1, 5, 1, 2, 1, 14, 1, 5, 1, 5, 2, 2, 1, 15, 2, 2, 5, 4, 1, 4, 1, 51]
+K = int(input())
+print (ans[K-1])
