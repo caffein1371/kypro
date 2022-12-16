631ffb9b087447be117b7197975cb9e9be01545b
cahein <incidents1371@gmail.com>
2022-12-17 08:02:59 +0900

    20221217

--- temp.py
+++ temp.py
@@ -3,8 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-11 30
-12 1
+1079
+
+
 
 
 
@@ -22,9 +23,13 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-M1,D1 = map(int,input().split())
-M2,D2 = map(int,input().split())
-if (abs(M2-M1)==1 or abs(M2-M1)==11) and abs(D1-D2)>1:
-    print (1)
-else:
-    print (0) 
+N = int(input())
+import math
+
+for i in range(1,50001):
+    
+    if math.floor(i*1.08)== N:
+        #print ((math.floor(i*1.08)))
+        print (i)
+        quit()
+print (":(")
