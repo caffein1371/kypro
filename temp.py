99090d224a85026df54daf5d32c90723ff831918
cahein <incidents1371@gmail.com>
2022-12-19 22:19:42 +0900

    20221219

--- temp.py
+++ temp.py
@@ -3,7 +3,11 @@ import io
 import sys
 
 _INPUT = """\
-725
+4 1 10
+10 7 5
+2 10
+
+
 
 
 
@@ -11,20 +15,18 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-X = int(input())
-if 400<=X<600:
-    print (8)
-elif 600<=X<800:
-    print (7)
-elif 800<=X<1000:
-    print (6)
-elif 1000<=X<1200:
-    print (5)
-elif 1200<=X<1400:
-    print (4)
-elif 1400<=X<1600:
-    print (3) 
-elif 1600<=X<1800:
-    print (2)
-elif 1800<=X<2000:
-    print (1)
+N,M,T = map(int,input().split())
+Alist = list(map(int,input().split()))
+X = []
+Y = []
+for i in range(M):
+    x,y = map(int,input().split())
+    Alist[x-1]-=y
+
+for i in range(len(Alist)):
+    T-=Alist[i]
+
+if T<=0:
+    print ("No")
+else:
+    print ("Yes")
