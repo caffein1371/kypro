9f77bc71bf9e84fc10108d9ee1fb79a5c9644f2d
cahein <incidents1371@gmail.com>
2022-12-19 12:53:47 +0900

    20221219

--- temp.py
+++ temp.py
@@ -3,14 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-2 3
-RDU
-LRU
-
-
-
-
-
+725
 
 
 
@@ -18,22 +11,20 @@ LRU
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-H,W = map(int,input().split())
-G = []
-c,r=0,0
-for i in range(H):
-    g = input()
-    G.append(g)
-while True:
-    if G[c][r]=="U" and c!=0:
-        c-=1
-    elif G[c][r]=="D" and c!=H-1:
-        c+=1
-    elif G[c][r]=="L" and r!=0:
-        r-=1
-    elif G[c][r]=="R" and r!=W-1:
-        r+=1
-    else:
-        break
-
-print (c+1,r+1)
+X = int(input())
+if 400<=X<600:
+    print (8)
+elif 600<=X<800:
+    print (7)
+elif 800<=X<1000:
+    print (6)
+elif 1000<=X<1200:
+    print (5)
+elif 1200<=X<1400:
+    print (4)
+elif 1400<=X<1600:
+    print (3) 
+elif 1600<=X<1800:
+    print (2)
+elif 1800<=X<2000:
+    print (1)
