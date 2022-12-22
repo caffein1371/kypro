07d91dd467503889aae0b80356e1a044609095b3
cahein <incidents1371@gmail.com>
2022-12-22 21:07:13 +0900

    20221222

--- temp.py
+++ temp.py
@@ -3,24 +3,26 @@ import io
 import sys
 
 _INPUT = """\
-7 2 5
-3
-
-
-
+4
+ooxx
+xoox
+xxxx
+xxxx
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A,B,C = map(int,input().split())
-K = int(input())
+N= int(input())
+S = []
+for i in range(N):
+    s = input()
+    S.append(s)
+ans = [["x" for j in range(N)]for i in range(N)]
+for i in range(N):
+    for j in range(N):
+        ans[j][i]=S[N-1-i][j]
 
-A1 = A*2
-B1 = B*2
-C1 = C*2
-
-if B1>A or C1>B:
-    print ("Yes")
-else:
-    print ("No")
+for i in range(N):
+    a = ans[i]
+    print ("".join(a))
