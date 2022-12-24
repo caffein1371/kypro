383c118b66277701ea6acddbf9c2d3467daee83d
cahein <incidents1371@gmail.com>
2022-12-24 21:44:11 +0900

    20221224

--- temp.py
+++ temp.py
@@ -3,9 +3,11 @@ import io
 import sys
 
 _INPUT = """\
-2 2
-.#
-#.
+1355506027
+
+
+
+
 
 
 
@@ -14,24 +16,7 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-H,W = map(int,input().split())
-S = []
-ans = 0
-for i in range(H):
-    s = input()
-    S.append(s)
-    for i in range(1,W):
-        if s[i]=="." and s[i-1]==".":
-            ans+=1
-#print (S)
-temp =[["" for i in range(H)] for j in range(W)]
-for i in range(len(S)):
-    for j in range(len(S[i])):
-        temp[j][i]= S[i][j]
-for i in range(len(temp)):
-    #print (temp[i])
-    for j in range(1,len(temp[i])):
-        if temp[i][j]=="." and temp[i][j-1]==".":
-            ans+=1
-
-print (ans)
+S = input()
+#print (len(S))
+temp = S.count("00")
+print (len(S)-temp)
