42341e227926f3c1c65e3e3a0173ddae0540e0dc
cahein <incidents1371@gmail.com>
2022-12-28 00:07:29 +0900

    20221228

--- temp.py
+++ temp.py
@@ -3,7 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-500
+4
+0 1 3 8
 
 
 
@@ -12,21 +13,14 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-ans = []
-count = 10
-while N!=0:
-    if N>2**count:
-        N = N%2**count
-        ans.append(2**count)
-    elif N==2:
-        N = N%2
-        ans.append(2)
-    elif N==1:
-        N = N%1
-        ans.append(1)
-    #print (N)
-    count-=1
 
-print (len(ans))
-for i in ans:
-    print (i)
+import math
+
+Alist = list(map(int,input().split()))
+n =0
+for i in range(len(Alist)):
+    if Alist[i]!=0:
+        n+=1
+
+#print (sum(Alist)/n)
+print(math.ceil(sum(Alist)/n))
