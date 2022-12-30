6176463fa08210c35e9fb0856c6721235fb4438d
cahein <incidents1371@gmail.com>
2022-12-30 22:21:14 +0900

    20221230

--- temp.py
+++ temp.py
@@ -3,25 +3,24 @@ import io
 import sys
 
 _INPUT = """\
-0 3 1 5
-
-
-
-
-
-
-
-
+5
+error
+2
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-L1,R1,L2,R2 = map(int,input().split())
-L = max(L1,L2)
-R = min(R1,R2)
-if R1<L2 or R2<L1:
-    print (0)
-else:
-    print (abs(L-R))
+N = int(input())
+S = input()
+K = int(input())
+
+temp = S[K-1]
+ans = []
+for i in range(len(S)):
+    if S[i]!=temp:
+        ans.append("*")
+    else:
+        ans.append(S[i])
+print ("".join(ans))
