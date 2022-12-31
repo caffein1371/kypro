f81f02a78b66428962d679760831d71c5470f692
cahein <incidents1371@gmail.com>
2022-12-31 23:54:01 +0900

    20221231

--- temp.py
+++ temp.py
@@ -3,24 +3,14 @@ import io
 import sys
 
 _INPUT = """\
-5
-error
-2
+111100
+
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
 S = input()
-K = int(input())
-
-temp = S[K-1]
-ans = []
-for i in range(len(S)):
-    if S[i]!=temp:
-        ans.append("*")
-    else:
-        ans.append(S[i])
-print ("".join(ans))
+print (S.count("1"))
