0e3a8115e1cf0f0889606b9a158c57bee95e0ead
cahein <incidents1371@gmail.com>
2023-01-02 13:33:05 +0900

    20220102

--- temp.py
+++ temp.py
@@ -3,26 +3,19 @@ import io
 import sys
 
 _INPUT = """\
-xyz
-abc
+90 30
+
+
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-O = input()
-E = input()
-temp = max(len(O),len(E))
-ans = []
-for i in range(temp):
-    try:
-        ans.append(O[i])
-    except:
-        pass
-    try:
-        ans.append(E[i])
-    except:
-        pass
-
-print ("".join(ans))
+n,x = map(int,input().split())
+temp = n//2
+if temp<x:
+    print (n-x)
+else:
+    print (x-1)
