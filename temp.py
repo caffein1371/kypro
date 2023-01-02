50aa10f6e71f43b5cfcb689d0b71e933feec65a9
cahein <incidents1371@gmail.com>
2023-01-02 13:44:03 +0900

    20230102

--- temp.py
+++ temp.py
@@ -3,9 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-90 30
-
-
+4 1 10
+10 7 5
+2 10
 
 
 
@@ -13,9 +13,19 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-n,x = map(int,input().split())
-temp = n//2
-if temp<x:
-    print (n-x)
-else:
-    print (x-1)
+N,M,T = map(int,input().split())
+Alist = list(map(int,input().split()))
+X = []
+Y = []
+for i in range(M):
+    x,y = map(int,input().split())
+    Alist[x-1]-=y
+
+#print (Alist)
+for i in range(len(Alist)):
+    T-=Alist[i]
+    if T<=0:
+        print ("No")
+        quit()
+print ("Yes")
+
