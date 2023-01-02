5af169a4946891339ae56a73f65396754f47884d
cahein <incidents1371@gmail.com>
2023-01-03 07:50:40 +0900

    20230103

--- temp.py
+++ temp.py
@@ -3,9 +3,14 @@ import io
 import sys
 
 _INPUT = """\
-4 1 10
-10 7 5
-2 10
+1222
+
+
+
+
+
+
+
 
 
 
@@ -13,19 +18,5 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,M,T = map(int,input().split())
-Alist = list(map(int,input().split()))
-X = []
-Y = []
-for i in range(M):
-    x,y = map(int,input().split())
-    Alist[x-1]-=y
-
-#print (Alist)
-for i in range(len(Alist)):
-    T-=Alist[i]
-    if T<=0:
-        print ("No")
-        quit()
-print ("Yes")
-
+N = input()
+print (N.count("2"))
