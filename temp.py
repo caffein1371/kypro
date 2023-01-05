d7356579d3620c18813b537056014ab32981b4d6
cahein <incidents1371@gmail.com>
2023-01-05 22:33:54 +0900

    20230105

--- temp.py
+++ temp.py
@@ -3,9 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-3
-5 6
-5 6 10
+abc
+
 
 
 
@@ -19,19 +18,9 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N= int(input())
-A,B = map(int,input().split())
-Plist = list(map(int,input().split()))
-temp1 = 0
-temp2 = 0
-temp3 = 0
-for i in Plist:
-    if i<=A:
-        temp1+=1
-    if A+1<=i<=B:
-        temp2+=1
-    if B+1<=i:
-        temp3+=1
-temp = min(temp1,temp2,temp3)
+S = input()
+if len(S)%2==0:
+    print (S)
+else:
+    print (S[::-1])
 
-print (temp)
