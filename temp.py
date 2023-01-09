19224ed0e90bad76d2c08891c448efa781a3d838
cahein <incidents1371@gmail.com>
2023-01-09 10:42:02 +0900

    20230109

--- temp.py
+++ temp.py
@@ -15,22 +15,21 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 T = int(input())
 import math
+from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 for i in range(T):
-    test = int(input())
-    p,q = 0,0
-
-    for j in range(2,int((test+1)**(1/3))+1):
-        #print (int((test+1)**(1/3)))
-        if test%j!=0:
-            continue
-        if (test/j)%j==0:
-            p = j
-            q = int (test/j/j)
-        else:
-            q = j
-            p = int(math.sqrt(test/j))
-        break
-    print (p,q)
+    N=int(input())
+    
+    #print (temp)
+    
+    for p in range(2,int(N**(1/3))+1):
+        if N%p==0:
+            q = N//p
+            if q%p==0:
+                print (p,N//(p**2))
+                break
+            else:
+                print (int(math.sqrt(q)),p)
+                break
 
         
 
