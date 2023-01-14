369dd916bbd40d05b9bc56d9706583e0c6277f6c
cahein <incidents1371@gmail.com>
2023-01-14 19:54:48 +0900

    20230114

--- temp.py
+++ temp.py
@@ -3,8 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-17
-
+4 3 2
 
 
 
@@ -13,8 +12,33 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-L = int(input())
-ans = 1
-from scipy.special import comb
+N,A,B = map(int,input().split())
+tiles = [['-' for i in range(N+1)] for j in range(N+1)]
+#print (tiles)
+for i in range(N+1):
+    if i%2==0:
+        for j in range(N+1):
+            if j%2==0:
+                tiles[i][j]='.'
+            else:
+                tiles[i][j]='#'
+    else:
+        for j in range(N+1):
+            if j%2==0:
+                tiles[i][j]='#'
+            else:
+                tiles[i][j]='.'
 
-print (comb(L-1,11,True))
+print (tiles)
+
+
+X = [['-' for i in range(A*N)] for j in range(B*N)]
+print (X)
+for i in range(A*N):
+    for j in range(B*N):
+        print (i,j)
+        print (round(i/A),round(i/B))
+        X[i][j]=tiles[round(i/A)][round(j/B)]
+
+for i in range(A*N):
+    print (''.join(X))
