04aba899ef0334fdc817e9c348ab326065fc0eb5
cahein <incidents1371@gmail.com>
2023-01-15 20:13:50 +0900

    20230115

--- temp.py
+++ temp.py
@@ -3,8 +3,13 @@ import io
 import sys
 
 _INPUT = """\
-100 200 50 20
-40 10
+5 50 100
+120
+-10
+-20
+-30
+70
+
 
 
 
@@ -12,10 +17,18 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A,B,C,K = map(int,input().split())
-S,T = map(int,input().split())
-temp = A*S+B*T
-if S+T>=K:
-    print (temp-(S+T)*C)
-else:
-    print (temp)
+N,S,T = map(int,input().split())
+W = int(input())
+A = []
+ans = 0
+if S<=W<=T:
+    ans+=1
+
+for i in range(N-1):
+    a = int(input())
+    W = W+a
+    if S<=W<=T:
+        ans+=1
+    if W<0:
+        W = 0
+print (ans)
