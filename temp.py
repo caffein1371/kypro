f949d8c0b414621e817f6060837a83f153656600
cahein <incidents1371@gmail.com>
2023-01-15 19:27:58 +0900

    20230115

--- temp.py
+++ temp.py
@@ -3,8 +3,12 @@ import io
 import sys
 
 _INPUT = """\
-4 3 2
-
+5
+newfile
+newfile
+newfolder
+newfile
+newfolder
 
 
 
@@ -12,33 +16,15 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,A,B = map(int,input().split())
-tiles = [['-' for i in range(N+1)] for j in range(N+1)]
-#print (tiles)
-for i in range(N+1):
-    if i%2==0:
-        for j in range(N+1):
-            if j%2==0:
-                tiles[i][j]='.'
-            else:
-                tiles[i][j]='#'
+N = int(input())
+F = []
+from collections import defaultdict
+d = defaultdict(int)
+for i in range(N):
+    f = input()
+    if d[f]==0:
+        print (f)
     else:
-        for j in range(N+1):
-            if j%2==0:
-                tiles[i][j]='#'
-            else:
-                tiles[i][j]='.'
-
-print (tiles)
-
-
-X = [['-' for i in range(A*N)] for j in range(B*N)]
-print (X)
-for i in range(A*N):
-    for j in range(B*N):
-        print (i,j)
-        print (round(i/A),round(i/B))
-        X[i][j]=tiles[round(i/A)][round(j/B)]
-
-for i in range(A*N):
-    print (''.join(X))
+        print (f+'('+str(d[f])+')')
+    d[f]+=1
+#print (d)
