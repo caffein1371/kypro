3381e63d5ff000e266ec68ae7a89f37dab5084df
cahein <incidents1371@gmail.com>
2023-01-15 20:29:52 +0900

    20230115

--- temp.py
+++ temp.py
@@ -3,12 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-5 50 100
-120
--10
--20
--30
-70
+ABBCCCDDDDEEEEEFFFFFF
+
 
 
 
@@ -17,18 +13,13 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,S,T = map(int,input().split())
-W = int(input())
-A = []
-ans = 0
-if S<=W<=T:
-    ans+=1
+S = input()
+import collections
+c = collections.Counter(S)
 
-for i in range(N-1):
-    a = int(input())
-    W = W+a
-    if S<=W<=T:
-        ans+=1
-    if W<0:
-        W = 0
-print (ans)
+ans = ['A','B','C','D','E','F']
+for i in range(len(ans)):
+    if i!=len(ans)-1:
+        print (c[ans[i]],end =' ')
+    else:
+        print (c[ans[i]])
