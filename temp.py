4fc4115b64af472cf5d76964a33ea97e0d1e9816
cahein <incidents1371@gmail.com>
2023-01-15 22:37:16 +0900

    20230115

--- temp.py
+++ temp.py
@@ -3,23 +3,32 @@ import io
 import sys
 
 _INPUT = """\
-ABBCCCDDDDEEEEEFFFFFF
-
-
-
-
+6
+abcbac
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
+N = int(input())
 S = input()
-import collections
-c = collections.Counter(S)
-
-ans = ['A','B','C','D','E','F']
-for i in range(len(ans)):
-    if i!=len(ans)-1:
-        print (c[ans[i]],end =' ')
-    else:
-        print (c[ans[i]])
+j = 0
+l = 0
+temp=0
+from collections import defaultdict
+d = defaultdict(list)
+for i in range(N):
+    for j in range(i+1,N):
+        #print (i,j,S[i],S[j])
+        d[abs(i-j)].append([S[i],S[j]])
+#print (d)
+l = 0
+for v in range(1,len(d)+1):
+    l = 0
+    for i in range(len(d[v])):
+        #print (d[v][i])
+        if d[v][i][0]!=d[v][i][1]:
+            l+=1
+        else:
+            break
+    print (l)
