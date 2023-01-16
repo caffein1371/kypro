c0a73bfaebb7ec0d3a67321291989dc122d971ba
cahein <incidents1371@gmail.com>
2023-01-16 17:19:57 +0900

    20230116

--- temp.py
+++ temp.py
@@ -10,25 +10,19 @@ abcbac
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
+N= int(input())
 S = input()
-j = 0
-l = 0
-temp=0
-from collections import defaultdict
-d = defaultdict(list)
-for i in range(N):
-    for j in range(i+1,N):
-        #print (i,j,S[i],S[j])
-        d[abs(i-j)].append([S[i],S[j]])
-#print (d)
-l = 0
-for v in range(1,len(d)+1):
-    l = 0
-    for i in range(len(d[v])):
-        #print (d[v][i])
-        if d[v][i][0]!=d[v][i][1]:
-            l+=1
-        else:
+
+#iは何個スキップするか
+#MaxでN-1スキップ
+for i in range(1,N):
+    for j in range(1,N+1):
+        #最後まで見つからなかった場合
+        if i+j>N:
+            print (j-1)
             break
-    print (l)
+        #途中で見つかった場合
+        #print (S[j-1],S[i+j-1])
+        if S[j-1]==S[i+j-1]:
+            print (j-1)
+            break
