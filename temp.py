e8a9f2f96661886e4081c93e5154cf9d3f215b95
cahein <incidents1371@gmail.com>
2023-01-18 20:57:14 +0900

    20230118

--- temp.py
+++ temp.py
@@ -3,26 +3,36 @@ import io
 import sys
 
 _INPUT = """\
-6
-abcbac
+5 2
+1 3
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N= int(input())
-S = input()
+N,M = map(int,input().split())
+Alist = list(map(int,input().split()))
 
-#iは何個スキップするか
-#MaxでN-1スキップ
-for i in range(1,N):
-    for j in range(1,N+1):
-        #最後まで見つからなかった場合
-        if i+j>N:
-            print (j-1)
-            break
-        #途中で見つかった場合
-        #print (S[j-1],S[i+j-1])
-        if S[j-1]==S[i+j-1]:
-            print (j-1)
-            break
+
+if N==1 and M==0:
+    print (1)
+    quit()
+elif N==M:
+    print (0)
+    quit()
+else:
+    Alist.append(0)
+    Alist.append(N+1)
+    Alist = list(sorted(set(Alist)))
+    TEMP =[]
+    for i in range(len(Alist)-1):
+        temp = Alist[i+1]-Alist[i]-1
+        #print (temp)
+        if temp!=0:
+            TEMP.append(temp)
+a = min(TEMP)
+ans = 0
+for i in range(len(TEMP)):
+    ans+=-(-TEMP[i]//a)
+
+print (ans)
