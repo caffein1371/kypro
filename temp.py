24bb3a8f74ef65150338a9dbba529fdeffa86e9c
cahein <incidents1371@gmail.com>
2023-01-20 21:06:47 +0900

    20230120

--- temp.py
+++ temp.py
@@ -12,21 +12,20 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
 mod = 998244353
-
-dp =[[0 for i in range(11)] for j in range(N+1)]
-print (dp)
+dp =[[0 for i in range(10)] for j in range(N+1)]
+#print (dp)
 for i in range(1,10):
     dp[1][i]=1
 
 for d in range(2,N+1):
     for i in range(1,10):
-        for j in range(max(1,i-1),min(10,i+1)):
-            print (d,i)
-            print (d,j)
+        for j in range(max(1,i-1),min(10,i+2)):
+            #print (d,i)
+            #print (d,j)
             dp[d][j]+=dp[d-1][i]
             dp[d][j]%=mod
 
-print (dp)
+#print (dp)
 ans = 0
 for i in range(1,10):
     ans+=dp[N][i]
