f41cef46f251a6f463050ed47dac026862da4637
cahein <incidents1371@gmail.com>
2023-01-23 23:15:55 +0900

    20230123

--- temp.py
+++ temp.py
@@ -3,8 +3,10 @@ import io
 import sys
 
 _INPUT = """\
-5 1 2
-rrefa
+3 1001
+1 1
+2 1
+100 10
 
 
 
@@ -14,14 +16,33 @@ rrefa
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-n, a, b = map(int, input().split())
-s = input()
-s += s
-ans = 1 << 60
-for i in range(n):
-    tmp = a * i
-    for j in range(n // 2):
-        if s[i + j] != s[i + n - 1 - j]:
-            tmp += b
-    ans = min(ans, tmp)
-print(ans)
+#個数制限付き部分和問題
+N,X = map(int,input().split())
+
+INF = 1<<29
+A = []
+B = []
+for i in range(N):
+    a,b = map(int,input().split())
+    A.append(a)
+    B.append(b)
+
+
+dp =[[INF for i in range(10011)] for j in range(111)]
+dp[0][0]=0
+
+for i in range(N):
+    for j in range(X+1):
+        #print (i,j)
+        if dp[i][j]<INF:
+            dp[i+1][j]=0
+        if j>=A[i]:
+            if dp[i][j-A[i]]<INF:
+                dp[i+1][j]=min(dp[i+1][j],1)
+            if dp[i+1][j-A[i]]<B[i]:
+                dp[i+1][j]=min(dp[i+i][j],dp[i+1][j-A[i]]+1)
+
+if dp[N][X]<INF:
+    print ('Yes')
+else:
+    print ('No')
