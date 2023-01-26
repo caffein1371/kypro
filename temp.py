1b43e6d875b6a8d861626d2fb60742488623bd70
cahein <incidents1371@gmail.com>
2023-01-26 22:27:49 +0900

    20230126

--- temp.py
+++ temp.py
@@ -3,12 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-3 1001
-1 1
-2 1
-100 10
-
-
+4 10
+6 1 2 7
 
 
 
@@ -16,33 +12,9 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-#個数制限付き部分和問題
-N,X = map(int,input().split())
-
-INF = 1<<29
-A = []
-B = []
-for i in range(N):
-    a,b = map(int,input().split())
-    A.append(a)
-    B.append(b)
-
-
-dp =[[INF for i in range(10011)] for j in range(111)]
-dp[0][0]=0
-
-for i in range(N):
-    for j in range(X+1):
-        #print (i,j)
-        if dp[i][j]<INF:
-            dp[i+1][j]=0
-        if j>=A[i]:
-            if dp[i][j-A[i]]<INF:
-                dp[i+1][j]=min(dp[i+1][j],1)
-            if dp[i+1][j-A[i]]<B[i]:
-                dp[i+1][j]=min(dp[i+i][j],dp[i+1][j-A[i]]+1)
-
-if dp[N][X]<INF:
-    print ('Yes')
-else:
-    print ('No')
+N,K = map(int,input().split())
+alist = list(map(int,input().split()))
+temp = [0]+alist
+for i in range(1,N+1):
+    temp[i]+=temp[i-1]
+print (temp)
