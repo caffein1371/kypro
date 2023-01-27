05cb019b78f43e94b14a9a04d9955dc2bf7c3ee3
cahein <incidents1371@gmail.com>
2023-01-27 21:42:09 +0900

    20230127

--- temp.py
+++ temp.py
@@ -3,8 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-4 10
-6 1 2 7
+10 53462
+103 35322 232 342 21099 90000 18843 9010 35221 19352
 
 
 
@@ -14,7 +14,22 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N,K = map(int,input().split())
 alist = list(map(int,input().split()))
-temp = [0]+alist
-for i in range(1,N+1):
-    temp[i]+=temp[i-1]
-print (temp)
+
+res = 0
+right = 0 
+sume = 0
+for left in range(N):
+    #print (left)
+    while (right<N and sume<K):
+        sume+=alist[right]
+        right+=1
+    
+    #print (sume)
+    res+=right-left
+    if right==left:
+        right+=1
+    else:
+        sume-=alist[left]
+
+    #print (right)
+print (res)
