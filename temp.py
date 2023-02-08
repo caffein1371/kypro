ee2cb7dba12c68e3b22abb95ee1f30814df166dc
cahein <incidents1371@gmail.com>
2023-02-08 19:56:10 +0900

    20230208

--- temp.py
+++ temp.py
@@ -3,9 +3,12 @@ import io
 import sys
 
 _INPUT = """\
-3
-1 2 3
-
+100 5
+1
+23
+45
+67
+89
 
 
 
@@ -13,16 +16,20 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N  = int(input())
-wlist = list(map(int,input().split()))
+N,M = map(int,input().split())
+A = []
+mod = 1000000007
 
-sumans = sum(wlist)
-ans = 0
-temp = sumans
-for i in range(N):
-  ans += wlist[i]
-  if abs(ans-(sumans-ans))>temp:
-    break
-  temp = abs(ans-(sumans-ans))
-    
-print (temp)
+A = [True for i in range(N+3)]
+for i in range(M):
+    a = int(input())
+    A[a]=False
+
+dp=[0 for i in range(N+3)]
+dp[0]=1
+for n in range(0,N):
+    if A[n+1]==True:
+        dp[n+1]+=dp[n]
+    if A[n+2]==True:
+        dp[n+2]+=dp[n]
+print (dp[N]%mod)
