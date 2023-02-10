ab0093618051a516a999be2a981bc217fe89e79f
cahein <incidents1371@gmail.com>
2023-02-10 23:49:54 +0900

    20230210

--- temp.py
+++ temp.py
@@ -3,12 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-100 5
-1
-23
-45
-67
-89
+4
 
 
 
@@ -16,13 +11,15 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,K = map(int,input().split())
-Alist = list(map(int,input().split()))
+N = int(input())
 
-ans = 1
-for i in range(K):
-    ans*=Alist[i]
-for i in range(K,N):
-    
-    
+temp = 1
+ans = []
+for i in range(1,N):
+    ans.append(i)
 
+temp = ans.copy()
+
+ans.append(N)
+ans+=(temp[::-1])
+print (ans)
