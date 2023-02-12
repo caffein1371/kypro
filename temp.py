cb6a783eea180338aafb9a95416e371f20944145
cahein <incidents1371@gmail.com>
2023-02-13 02:07:18 +0900

    20230213

--- temp.py
+++ temp.py
@@ -3,8 +3,19 @@ import io
 import sys
 
 _INPUT = """\
-10 6
-1 2 3 7 8 9
+6 6
+3
+2 3 6
+3
+2 4 6
+2
+3 6
+3
+1 5 6
+3
+1 3 6
+2
+1 4
 
 
 
@@ -12,23 +23,32 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N,M = map(int,input().split())
-Alist = list(map(int,input().split()))
-tmp = [i for i in range(1,N+1)]
-if M==0:
-    print (*tmp)
-    quit()
+A = []
+for i in range(M):
+    c = int(input())
+    alist = list(map(int,input().split()))
+    A.append(alist)
+#print (A)
+an = 0
+tmp =[]
+for i in range(2**M):
+    ans = []
+    for j in range(M):
+        #ans = []
+        if ((i>>j)&1):
+            #print (A[j])
+            for k in A[j]:
+                #print (k)
+                ans.append(k)
+            tmp = set(ans)
+            #print (tmp)
+            if len(tmp)==N:
+                an+=1
+                break
+            #print ()
+            tmp = []
 
-flag = False
+print (an)
 
-ans = [0]
-for i in range(N):
-    if i+1 not in Alist:
-        #レ点がついていないもの
-        ans.append(i+1)
-
-print (*ans)
-for i in range(len(ans)-1):
-    for j in range(ans[i+1]-ans[i]):
-        print (ans[i+1]-j,end=' ')
 
     
