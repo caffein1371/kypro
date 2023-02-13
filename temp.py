aaef480a058844d8fafc92558b43735ee74e9695
cahein <incidents1371@gmail.com>
2023-02-14 08:26:00 +0900

    20230214

--- temp.py
+++ temp.py
@@ -3,38 +3,57 @@ import io
 import sys
 
 _INPUT = """\
-4
-3
-4
-1
-2
+10
+1 8 4 15 7 5 7 5 8 0
+20
+2 7 0
+3 7
+3 8
+1 7
+3 3
+2 4 4
+2 4 9
+2 10 5
+1 10
+2 4 2
+1 10
+2 3 1
+2 8 11
+2 3 14
+2 1 9
+3 8
+3 8
+3 1
+2 6 5
+3 7
+
+
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-A = []
+"""AtCoder Beginner Contest 278 D"""
+from collections import defaultdict
 
-for i in range(N):
-    a = int(input())
-    A.append(a)
-ans=0
-cur = 1
-flag = False
-while ans<100001:
-    #print (A[A[i]-1]==2)
-    cur = A[cur-1]
-    #print (cur)
+n = int(input())
+a = list(map(int, input().split()))
+g = 0
+base = 0
+diff = defaultdict(int)
 
-    if cur==2:
-        flag=True
-        break
-    ans+=1
-
-
-if flag:
-    print (ans+1)
-else:
-    print (-1)
+q = int(input())
+for i in range(q):
+    t = list(map(int, input().split()))
+    if t[0] == 1:
+        g += 1
+        base = t[1]
+    elif t[0] == 2:
+        diff[(g, t[1])] += t[2]
+    else:
+        if g > 0:
+            print(base + diff[(g, t[1])])
+        else:
+            print(a[t[1] - 1] + diff[(g, t[1])])
