05b0ee3c8642ec60d0c607a7bc6c98cc81aedc3a
cahein <incidents1371@gmail.com>
2023-02-13 14:51:10 +0900

    20230213

--- temp.py
+++ temp.py
@@ -3,52 +3,52 @@ import io
 import sys
 
 _INPUT = """\
-6 6
-3
-2 3 6
-3
-2 4 6
-2
-3 6
-3
-1 5 6
-3
-1 3 6
-2
-1 4
+7
+3 4 6 7 8 9 10
+
+
+
+
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,M = map(int,input().split())
-A = []
-for i in range(M):
-    c = int(input())
-    alist = list(map(int,input().split()))
-    A.append(alist)
-#print (A)
-an = 0
-tmp =[]
-for i in range(2**M):
-    ans = []
-    for j in range(M):
-        #ans = []
-        if ((i>>j)&1):
-            #print (A[j])
-            for k in A[j]:
-                #print (k)
-                ans.append(k)
-            tmp = set(ans)
-            #print (tmp)
-            if len(tmp)==N:
-                an+=1
-                break
-            #print ()
-            tmp = []
+N = int(input())
+Xlist = list(map(int,input().split()))
+import functools
+import math
+def sieve_of_eratosthenes(n):
+    prime = [True for i in range(n+1)]
+    prime[0] = False
+    prime[1] = False
 
-print (an)
+    sqrt_n = math.ceil(math.sqrt(n))
+    for i in range(2, sqrt_n):
+        if prime[i]:
+            for j in range(2*i, n+1, i):
+                prime[j] = False
 
+    return prime
+prime = []
+Y = sieve_of_eratosthenes(50)
+for p in range(len(Y)):
+    if Y[p]:
+        prime.append(p)
+ans = []
+for i in range(len(Xlist)):
+    for j in range(len(prime)):
+        if Xlist[i]%prime[j]==0:
+            ans.append(prime[j])
+            break
+#print(ans)
 
-    
+import math
+def my_lcm_base(x, y):
+    return (x * y) // math.gcd(x, y)
+
+def my_lcm(*integers):
+    return functools.reduce(my_lcm_base, integers)
+print (my_lcm(*ans))
