43beda1828deca030aeb42103edea30fdded36ef
cahein <incidents1371@gmail.com>
2023-02-13 21:43:42 +0900

    20230213

--- temp.py
+++ temp.py
@@ -3,13 +3,11 @@ import io
 import sys
 
 _INPUT = """\
-7
-3 4 6 7 8 9 10
-
-
-
-
-
+4
+3
+4
+1
+2
 
 
 
@@ -17,38 +15,26 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-Xlist = list(map(int,input().split()))
-import functools
-import math
-def sieve_of_eratosthenes(n):
-    prime = [True for i in range(n+1)]
-    prime[0] = False
-    prime[1] = False
+A = []
 
-    sqrt_n = math.ceil(math.sqrt(n))
-    for i in range(2, sqrt_n):
-        if prime[i]:
-            for j in range(2*i, n+1, i):
-                prime[j] = False
+for i in range(N):
+    a = int(input())
+    A.append(a)
+ans=0
+cur = 1
+flag = False
+while ans<100001:
+    #print (A[A[i]-1]==2)
+    cur = A[cur-1]
+    #print (cur)
 
-    return prime
-prime = []
-Y = sieve_of_eratosthenes(50)
-for p in range(len(Y)):
-    if Y[p]:
-        prime.append(p)
-ans = []
-for i in range(len(Xlist)):
-    for j in range(len(prime)):
-        if Xlist[i]%prime[j]==0:
-            ans.append(prime[j])
-            break
-#print(ans)
+    if cur==2:
+        flag=True
+        break
+    ans+=1
 
-import math
-def my_lcm_base(x, y):
-    return (x * y) // math.gcd(x, y)
 
-def my_lcm(*integers):
-    return functools.reduce(my_lcm_base, integers)
-print (my_lcm(*ans))
+if flag:
+    print (ans+1)
+else:
+    print (-1)
