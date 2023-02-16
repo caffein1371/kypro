ae58c7abdb13fb6eda9cc8ad09abbc6a1d05566a
cahein <incidents1371@gmail.com>
2023-02-17 00:55:24 +0900

    20230217

--- temp.py
+++ temp.py
@@ -3,11 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-10 1
-
-
-
-
+1 1000000000
 
 
 
@@ -15,9 +11,19 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-t,N= map(int,input().split())
+t,N = map(int,input().split())
+
+temp = [1]*(100+t)
 ans = []
-import decimal
-ans = 0
-for i in range(1,t+1):
-    print (((t+100)//100)*i)
+for i in range(100):
+    #整数型の切り捨て除算を用いて、(100+t)×A を 100 で割る形でf(A) を計算する
+    temp[((t+100)*i)//100]=0
+#print (temp)
+for i in range(100+t):
+    if temp[i]==1:
+        ans.append(i)
+#print (ans)
+
+q,mod = divmod(N-1,t)
+answer = q*(t+100)+ans[mod]
+print (answer)
