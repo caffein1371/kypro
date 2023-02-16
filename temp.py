c4f3f38960b1e73fa718e5f063828f9cc6018d14
cahein <incidents1371@gmail.com>
2023-02-16 17:09:23 +0900

    20230216

--- temp.py
+++ temp.py
@@ -3,8 +3,10 @@ import io
 import sys
 
 _INPUT = """\
-3 1
-1 2
+10 1
+
+
+
 
 
 
@@ -13,52 +15,9 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-# UnionFind
-class UnionFind:
-    def __init__(self,n):
-        self.n=n
-        self.parent_size=[-1]*n
- 
-    def leader(self,a):
-        if self.parent_size[a]<0: return a
-        self.parent_size[a]=self.leader(self.parent_size[a])
-        return self.parent_size[a]
- 
-    def merge(self,a,b):
-        x,y=self.leader(a),self.leader(b)
-        if x == y: return 
-        if abs(self.parent_size[x])<abs(self.parent_size[y]):x,y=y,x
-        self.parent_size[x] += self.parent_size[y]
-        self.parent_size[y]=x
-        return 
- 
-    def same(self,a,b):
-        return self.leader(a) == self.leader(b)
- 
-    def size(self,a):
-        return abs(self.parent_size[self.leader(a)])
- 
-    def groups(self):
-        result=[[] for _ in range(self.n)]
-        for i in range(self.n):
-            result[self.leader(i)].append(i)
-        return [r for r in result if r != []]
-
-N,M = map(int,input().split())
-UF = UnionFind(N)
-for i in range(M):
-    a,b = map(int,input().split())
-    UF.merge(a-1,b-1)
-
+t,N= map(int,input().split())
 ans = []
-for i in range(N):
-    #print (UF.leader(i))
-    ans.append(UF.leader(i))
-ans = set(ans)
-#print (ans)
-
-
-if len(ans)==1:
-    print (0)
-else:
-    print (len(ans)-1)
+import decimal
+ans = 0
+for i in range(1,t+1):
+    print (((t+100)//100)*i)
