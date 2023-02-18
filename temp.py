4eb08364d0cef98a88451f884617ae54492132ec
cahein <incidents1371@gmail.com>
2023-02-19 00:23:01 +0900

    20230219

--- temp.py
+++ temp.py
@@ -3,13 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-2 10
-20 20
-
-
-
-
-
+-505 191 278
 
 
 
@@ -18,22 +12,20 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,x = map(int,input().split())
-Alist = list(map(int,input().split()))
-Alist = list(sorted(Alist))
-temp = 0
-ans=0
-Blist = []
-if sum(Alist)<x:
-        print (N-1)
-        quit()
+a,b,d = map(int,input().split())
+import math
+# print (math.radians(d))
 
-for i in range(len(Alist)):
-        if temp+Alist[i]<=x:
-                temp+=Alist[i]
-                ans+=1
-        else:
-                break
-print (ans)
+print (a*math.cos(math.radians(d))-b*math.sin(math.radians(d)),a*math.sin(math.radians(d))+b*math.cos(math.radians(d)))
 
+# d = math.radians(d)
+# print (d)
+# ans1 = a*math.cos(d)-b*math.sin(d)
+# ans2 = a*math.sin(d)+b*math.cos(d)
+# print (ans1,ans2)
 
+from math import sin, cos, radians
+
+# a, b, d = map(int, input().split())
+# d = radians(d)
+# print(a * cos(d) - b * sin(d), a * sin(d) + b * cos(d))
