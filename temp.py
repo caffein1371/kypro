c01101ed34d0be3f4f291e7d0efd70f70d858bf1
cahein <incidents1371@gmail.com>
2023-02-19 23:30:13 +0900

    20230209

--- temp.py
+++ temp.py
@@ -3,7 +3,10 @@ import io
 import sys
 
 _INPUT = """\
--505 191 278
+7 3
+2 0 2 3 2 1 9
+
+
 
 
 
@@ -12,20 +15,14 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-a,b,d = map(int,input().split())
-import math
-# print (math.radians(d))
+N,K = map(int,input().split())
+Alist = list(map(int,input().split()))
+Alist = sorted(set(Alist))
+ans = -1
 
-print (a*math.cos(math.radians(d))-b*math.sin(math.radians(d)),a*math.sin(math.radians(d))+b*math.cos(math.radians(d)))
-
-# d = math.radians(d)
-# print (d)
-# ans1 = a*math.cos(d)-b*math.sin(d)
-# ans2 = a*math.sin(d)+b*math.cos(d)
-# print (ans1,ans2)
-
-from math import sin, cos, radians
-
-# a, b, d = map(int, input().split())
-# d = radians(d)
-# print(a * cos(d) - b * sin(d), a * sin(d) + b * cos(d))
+import itertools
+flag = False
+for i in range(len(Alist)):
+    if i!=Alist[i]:
+        print (i-1)
+        quit()
