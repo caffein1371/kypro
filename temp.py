d0d041ad3afa5899b2ba1ae21f4f3ea89810f528
cahein <incidents1371@gmail.com>
2023-02-21 23:44:33 +0900

    20230221

--- temp.py
+++ temp.py
@@ -3,14 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-5 2 4
-2
-4
-6
-8
-10
-
-
+6 5
+8 -3 5 7 0 -4
 
 
 
@@ -18,25 +12,17 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,A,B = map(int,input().split())
-#全体にPかけた場合，平均はP倍，最大ー最小はP倍
-#全体にQを足した場合，平均はQ足され，最大ー最小は変わらない
-#最大ー最小はP倍によって左右される
-P = 0 
-Q = 0
-temp1 = 0
-temp2 = 10**9
-temp3 = 0
+N,K = map(int,input().split())
+Alist = list(map(int,input().split()))
+ans = 0
+tmp = [0 for i in range(N+1)]
 for i in range(N):
-        s = int(input())
-        temp1+=s
-        temp2=min(temp2,s)
-        temp3=max(temp3,s)
-temp4 = temp3-temp2
-if temp4==0:
-        print (-1)
-        quit()
-P = B/temp4
-Q = A-(temp1/N)*P
+        tmp[i+1]=tmp[i]+Alist[i]
+from collections import defaultdict
+mp = defaultdict(int)
 
-print (P,Q)
+for r in range(1,N+1):
+        mp[tmp[r-1]]+=1
+        ans+=mp[tmp[r]-K]
+
+print (ans)
