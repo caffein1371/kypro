f897099e5242d1bd241fa862e76f4cd3ed745b11
cahein <incidents1371@gmail.com>
2023-02-24 13:21:23 +0900

    20230224

--- temp.py
+++ temp.py
@@ -3,7 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-99
+9
 
 
 
@@ -11,23 +11,5 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-# N = input()
-# ans = 0
-# if len(N)>1:
-#         if N[0]!='9':
-#                 ans+=int(N[0])-1
-#                 ans+=len(N[1::])*9
-#         if N[0]=='9' :
-#                 ans+=8
-#                 ans+=len(N[1::])*9
-#         print (ans)
-# else:
-#         print (int(N))
-
-N=int(input())
-S=str(N)
-c=0
-for s in S:
-  c+=int(s)
-d=(len(S)-1)*9+int(S[0])-1
-print(max(c,d))
+N = int(input())
+print (10**N+7)
