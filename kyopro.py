2cf32d9278ef39607677cf93c9ffa87796276995
cahein <incidents1371@gmail.com>
2023-02-26 01:21:41 +0900

    20230226

--- kyopro.py
+++ kyopro.py
@@ -3,29 +3,14 @@ import io
 import sys
 
 _INPUT = """\
-4
-unagi 20
-usagi 13
-snuke 42
-smeke 7
+1000000000 1000000000 1000000000
+
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-d = dict()
-temp = 0
-for i in range(N):
-    S,P = map(str,input().split())
-    #data = (S,int(P))
-    temp+= int(P)
-    d[S]=int(P)
-d = sorted(d.items(),key=lambda x:x[1],reverse=True)
-#print (d)
-#print (temp)
-if d[0][1]>temp/2:
-    print (d[0][0])
-else:
-    print ('atcoder')
+A,B,C = map(int,input().split())
+print (A*B*C%(10**9+7))
