43649728e73c1e418132efab2b0e478c931343c5
cahein <incidents1371@gmail.com>
2023-02-26 08:12:20 +0900

    20230226

--- temp.py
+++ temp.py
@@ -3,8 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-5
-4 2 5 1 3
+18278
+
 
 
 
@@ -15,11 +15,27 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-Plist = list(map(int,input().split()))
-temp = float('inf')
-ans = 0
-for i in Plist:
-  if i < temp:
-    temp = i
-    ans += 1
-print (ans)
+abc= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+mod = 26
+amari = -1
+iter = 0
+# if N==1:
+#         print ('a')
+#         exit()
+
+# while mod**iter<N:
+#         iter+=1
+# #print (iter)
+ans = []
+# for i in range(iter,-1,-1):
+#         #print (i)
+#         N,temp = divmod(N,mod)
+#         temp-=1
+#         ans.append(abc[temp])
+        
+while N!=0:
+        N-=1
+        M = N%mod
+        ans.append(abc[M])
+        N=N//mod
+print(''.join(ans[::-1]))
