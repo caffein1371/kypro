75357087a85c1854066607d2305bb2c25dd418cc
cahein <incidents1371@gmail.com>
2023-02-27 07:42:43 +0900

    20230227

--- temp.py
+++ temp.py
@@ -3,8 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-5
-RLURU
+3 5
+
 
 
 
@@ -12,31 +12,6 @@ RLURU
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-S = input()
-x,y = 0,0
-flag = False
-ans = ([[0,0]])
-iter = 1
-for i in range(N):
-        if S[i]=='U':
-                y+=1
-        elif S[i]=='R':
-                x+=1
-        elif S[i]=='L':
-                x-=1
-        elif S[i]=='D':
-                y-=1
-        
-        # if [x,y] in ans :
-        #         flag = True
-        #         break
-        ans.append([x,y])
-        iter+=1
-arr = list(map(list, set(map(tuple, ans))))
-# print (arr)
-# print (iter)
-if iter!=len(arr):
-        print ('Yes')
-else:
-        print ('No')
+R,D = map(int,input().split())
+import math
+print (R*R*math.pi*D*2*math.pi)
