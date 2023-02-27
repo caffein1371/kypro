a393c2709ef608de0809f1c365b3341f084155d2
cahein <incidents1371@gmail.com>
2023-02-28 00:00:19 +0900

    20230228

--- temp.py
+++ temp.py
@@ -3,8 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-3 5
-
+217
 
 
 
@@ -12,6 +11,18 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-R,D = map(int,input().split())
-import math
-print (R*R*math.pi*D*2*math.pi)
+X = int(input())
+bag = [100,101,102,103,104,105]
+
+dp = [False for i in range(10**5+1000)]
+dp[0]=True
+
+for i in range(X):
+        if dp[i]:
+                for j in range(6):
+                        dp[i+j+100]=True
+
+if dp[X]:
+        print (1)
+else:
+        print (0)
