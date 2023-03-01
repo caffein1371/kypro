cf945a1879dd23672ac0d539a2276c2820acb2fe
cahein <incidents1371@gmail.com>
2023-03-01 20:28:58 +0900

    20230301

--- kyopro.py
+++ kyopro.py
@@ -3,19 +3,36 @@ import io
 import sys
 
 _INPUT = """\
-0011
+11
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-S= input()
-#0と1でしか構成されないため、最後に残るのは0だけ、または1のみ
-#01または10の組み合わせで消えていくので011111のときは１回
-#01または10の組み合わせ数はmin(0の数,1の数)×2個
-num0 = S.count('0')
-num1 = S.count('1')
-ans = min(num0,num1)*2
-    
-print (ans)
+n = int(input())
+#n =int(input())
+#nまでの値をpに格納
+p = [i for i in range(n)]
+#2以降の倍数を0で初期化
+for i in p[3:]:
+     if p[i] % 2 == 0:
+         p[i] = 0
+#調べたい値の平方根を求める
+root_n = n ** 0.5
+#平方根以下の値
+for i in range(3, n):
+     if i > root_n:
+          break
+     if p[i] != 0:
+          for j in range(i, n , 2):
+               if i * j >= n :
+                    break
+               p[i * j] = 0
+#0が入っているものは１つになる
+#[2:]で0,1が消える
+plist = sorted(list(set(p)))[2:]
+plistn = len(plist)
+
+#print(plist)
+print(plistn)
