438445fbd2cc4a16115f375ec1efe66249359e74
cahein <incidents1371@gmail.com>
2023-03-01 15:29:58 +0900

    20230301

--- temp.py
+++ temp.py
@@ -3,16 +3,8 @@ import io
 import sys
 
 _INPUT = """\
-8 3
-2 6 8
--17683 17993
-93038 47074
-58079 -57520
--41515 -89802
--72739 68805
-24324 -73073
-71049 72103
-47863 19268
+2 3 4
+
 
 
 
@@ -22,22 +14,6 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,K = map(int,input().split())
-Alist = list(map(int,input().split()))
-XY = []
-
-for i in range(N):
-        x,y = map(int,input().split())
-        XY.append([x,y])
-#print (XY)
-import math
-ans = 0
-#全員とあかりを持っている人の距離の最小を計算し，
-#その中からmaxを取ることによって全員をカバーするための値を算出する
-for i in range(N):
-        temp = float('inf')
-        for j in range(len(Alist)):
-                temp = min(temp,math.sqrt((XY[i][0]-XY[Alist[j]-1][0])*(XY[i][0]-XY[Alist[j]-1][0])+(XY[i][1]-XY[Alist[j]-1][1])*(XY[i][1]-XY[Alist[j]-1][1])))
-        ans = max(temp,ans)
-print (float(ans))
-                        
+N,X,Y = map(int,input().split())
+dp = [False for i in range(N)]
+     
