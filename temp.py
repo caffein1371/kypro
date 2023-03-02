f97cf61687076980f59f20fc000ebe137fdd9dfb
cahein <incidents1371@gmail.com>
2023-03-02 23:02:25 +0900

    20230302

--- temp.py
+++ temp.py
@@ -3,17 +3,24 @@ import io
 import sys
 
 _INPUT = """\
-2 3 4
-
-
-
-
+10
+5 4 3 2 1 0 7 7 6 6
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,X,Y = map(int,input().split())
-dp = [False for i in range(N)]
-     
+N = int(input())
+plist = list(map(int,input().split()))
+#2*10**5
+temp = [0 for i in range(210001)]
+iter =0
+for i in range(N):
+        #print (plist[:i+1])
+        temp[plist[i]]+=1
+        #iterから始める
+        #tempが0であれば，whileから抜ける
+        while temp[iter]!=0:
+                iter+=1
+        print (iter)
