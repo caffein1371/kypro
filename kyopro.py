1bc85dacffc92349cba54e101fea68a35de68884
cahein <incidents1371@gmail.com>
2023-03-06 23:20:12 +0900

    20230306

--- kyopro.py
+++ kyopro.py
@@ -3,15 +3,25 @@ import io
 import sys
 
 _INPUT = """\
-B c
+hello
+3
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-C,c = map(str,input().split())
-if c.upper()==C:
-    print ('Yes')
-else:
-    print ('No')
+s= input()
+k = int(input())
+
+import itertools
+ans = 0
+if len(s)<k:
+    print (ans)
+    quit()
+temp = []
+for i in range(0,len(s)-k):
+    temp.append(s[i:i+k])
+ans = len(set(temp))
+print (ans)
