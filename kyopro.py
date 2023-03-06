4d005d8ebc4dedeeb72f2b95a46c6055c6c9ccf7
cahein <incidents1371@gmail.com>
2023-03-06 23:04:11 +0900

    20230306

--- kyopro.py
+++ kyopro.py
@@ -3,23 +3,15 @@ import io
 import sys
 
 _INPUT = """\
-5
-1 4 3 5 8
-
+B c
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-Alist = list(map(int,input().split()))
-Alist = sorted(Alist)
-import itertools
-ans = -1
-#print (Alist[-3:])
-for v in itertools.permutations(Alist[-3:],3):
-    temp = int(''.join(str(v[0])+str(v[1])+str(v[2])))
-    #print (temp)
-    ans = max(temp,ans)
-print (ans)
+C,c = map(str,input().split())
+if c.upper()==C:
+    print ('Yes')
+else:
+    print ('No')
