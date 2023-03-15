837706af5d9ea160893a372d8e76c0fa9ce54683
cahein <incidents1371@gmail.com>
2023-03-15 23:31:12 +0900

    20230315

--- temp.py
+++ temp.py
@@ -3,24 +3,16 @@ import io
 import sys
 
 _INPUT = """\
-34
-ABABAAABACDDDABADFFABABDABFAAABFAA
+P
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-S = input()
-ans = 0
-for i in range(N):
-        if S[i]=='A':
-                ans+=4
-        elif S[i]=='B':
-                ans+=3
-        elif S[i]=='C':
-                ans+=2
-        elif S[i]=='D':
-                ans+=1
-print (float(ans/N))
+c = input()
+ans = ['O','P','L','K']
+if c in ans:
+        print ('Right')
+else:
+        print ('Left')
