e922b0823495c3bb328eec263d3a4800cc88bdd4
cahein <incidents1371@gmail.com>
2023-03-15 23:34:54 +0900

    20230315

--- temp.py
+++ temp.py
@@ -3,16 +3,18 @@ import io
 import sys
 
 _INPUT = """\
-P
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-c = input()
-ans = ['O','P','L','K']
-if c in ans:
-        print ('Right')
-else:
-        print ('Left')
+def func(n):
+        if n==0 or n==1:
+                return 100
+        elif n==2:
+                return 200
+        else:
+                return func(n-1)+func(n-2)+func(n-3)
+print (func(19))
