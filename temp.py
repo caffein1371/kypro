360b28e58a66243147596e380f8ae09e1d5be088
cahein <incidents1371@gmail.com>
2023-03-16 21:04:36 +0900

    20230316

--- temp.py
+++ temp.py
@@ -3,18 +3,15 @@ import io
 import sys
 
 _INPUT = """\
-
+xxxxxxxx
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-def func(n):
-        if n==0 or n==1:
-                return 100
-        elif n==2:
-                return 200
-        else:
-                return func(n-1)+func(n-2)+func(n-3)
-print (func(19))
+S = input()
+if S.count('x')>7:
+        print ('NO')
+else:
+        print ('YES')
