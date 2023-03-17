1d3b111be9a8131903510583f0b7ff86d1e7720b
cahein <incidents1371@gmail.com>
2023-03-17 16:53:42 +0900

    20230317

--- temp.py
+++ temp.py
@@ -3,14 +3,19 @@ import io
 import sys
 
 _INPUT = """\
-hihi
+4
+BRBR
+
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-S = input()
-if 'hi' in S:
+N = int(input())
+s = input()
+rc = s.count('R')
+bc = s.count('B')
+if rc>bc:
         print ('Yes')
 else:
         print ('No')
