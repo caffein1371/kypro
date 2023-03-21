1f98c8ee7a4ec3c2e2571aa49d95b3d6ef21845b
cahein <incidents1371@gmail.com>
2023-03-21 22:21:03 +0900

    20230321

--- kyopro.py
+++ kyopro.py
@@ -3,18 +3,20 @@ import io
 import sys
 
 _INPUT = """\
-5
-1 2 3 5 6
+2 3
+0 1 2
+0 0 3
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-Alist = list(map(int,input().split()))
-ans = []
-for i in range(N):
-    if Alist[i]%2==0:
-        ans.append(Alist[i])
-print (*ans)
+H,W = map(int,input().split())
+ABC = ['.','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+for i in range(H):
+    Alist = list(map(int,input().split()))
+    ans = []
+    for j in range(W):
+        ans.append(ABC[Alist[j]])
+    print (''.join(ans))
