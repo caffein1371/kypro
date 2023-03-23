466e3c3fd11aab2ed6f82958ab209326400696aa
cahein <incidents1371@gmail.com>
2023-03-23 13:31:12 +0900

    20230323

--- kyopro.py
+++ kyopro.py
@@ -3,20 +3,37 @@ import io
 import sys
 
 _INPUT = """\
-2 3
-0 1 2
-0 0 3
+4 3
+3 14 15 92
+6 53 58
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-H,W = map(int,input().split())
-ABC = ['.','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-for i in range(H):
-    Alist = list(map(int,input().split()))
-    ans = []
-    for j in range(W):
-        ans.append(ABC[Alist[j]])
-    print (''.join(ans))
+N,M = map(int,input().split())
+Alist = list(map(int,input().split()))
+Blist = list(map(int,input().split()))
+from collections import defaultdict
+c = defaultdict(int)
+
+Clist = Alist+Blist
+Clist = sorted(Clist)
+#print (Clist)
+for i in range(N+M):
+    c[Clist[i]]=i+1
+
+#print (c)
+#print (b)
+for i in range(N):
+    if N-1!=i:
+        print (c[Alist[i]],end =' ')
+    elif N-1==i:
+        print (c[Alist[i]])
+for j in range(M):
+    if M-1!=j:
+        print (c[Blist[j]],end =' ')
+    elif M-1==j:
+        print (c[Blist[j]])
