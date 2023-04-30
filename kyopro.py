08902131d708e1771060ea6fe0e9e5079994b4ad
cahein <incidents1371@gmail.com>
2023-04-30 19:34:32 +0900

    20230430

--- kyopro.py
+++ kyopro.py
@@ -3,26 +3,65 @@ import io
 import sys
 
 _INPUT = """\
-10
-in that1 case you1 should print yes and1 not1 no
-
-
-
+4 3
+..#
+...
+.#.
+...
+#..
+...
+.#.
+...
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-S =input()
-Wlist = S.split()
-#print (Wlist)
-temp = ['and','not','that','the','you']
-for i in range(N):
-    for j in range(5):
-        if Wlist[i]==temp[j]:
-            print ('Yes')
-            #print (i,j)
-            quit()
-print ('No')
+H,W = map(int,input().split())
+A = []
+ansA = []
+for i in range(H):
+    Alist = input()
+    ans = 0
+    for j in range(W):
+        if Alist[j]=='#':
+            ans+=1
+    ansA.append(ans)
+    A.append(Alist)
+#print (A)
+#print (ansA)
+ansA1 = []
+for i in range(W):
+    ans=0
+    for j in range(H):
+        if A[j][i]=='#':
+            ans+=1
+    ansA1.append(ans)
+#print (ansA1)
+ansB = []
+B = []
+for i in range(H):
+    Blist = input()
+    ans = 0
+    for j in range(W):
+        if Blist[j]=='#':
+            ans+=1
+    ansB.append(ans)
+    B.append(Blist)
+ansB1 = []
+for i in range(W):
+    ans=0
+    for j in range(H):
+        if B[j][i]=='#':
+            ans+=1
+    ansB1.append(ans)
+#print (ansB1)
+
+
+#print (B)
+#print (ansB)
+if sorted(ansA)==sorted(ansB) or sorted(ansA1)==sorted(ansB1):
+    print ('Yes')
+else:
+    print ('No')
