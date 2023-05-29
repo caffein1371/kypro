e7003721ccda181dfe595925b4e9990fdb05276d
cahein <incidents1371@gmail.com>
2023-05-29 14:56:16 +0900

    20230529

--- temp.py
+++ temp.py
@@ -3,45 +3,22 @@ import io
 import sys
 
 _INPUT = """\
--1 0 3
-
-
-
+........
+........
+........
+........
+........
+.*......
+........
+........
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A,B,C = map(int,input().split())
-gu = True
-if C%2!=0:
-    gu = False
-import math
-
-if gu==True:
-    if abs(B)==abs(A):
-        print ('=')
-    elif abs(A)>abs(B):
-        print('>')
-    elif abs(B)>abs(A):
-        print ('<')
-else:
-    if A>=0 and B>=0:
-        if abs(B)==abs(A):
-            print ('=')
-        elif abs(A)>abs(B):
-            print('>')
-        elif abs(B)>abs(A):
-            print ('<')
-    elif A>=0 and B<0:
-        print ('>')
-    elif A<0 and B>=0:
-        print ('<')
-    elif A<0 and B<0:
-        if abs(B)==abs(A):
-            print ('=')
-        elif abs(A)<abs(B):
-            print('>')
-        elif abs(B)>abs(A):
-            print ('<')
+yoko = ['a','b','c','d','e','f','g','h','i']
+for i in range(8):
+    S = input()
+    if '*' in S:
+        print (str(yoko[S.find('*')])+str(8-i))
