559237350f3c2425c1304ac9b3f862181f706750
cahein <incidents1371@gmail.com>
2023-05-29 18:26:45 +0900

    20230529

--- temp.py
+++ temp.py
@@ -3,22 +3,48 @@ import io
 import sys
 
 _INPUT = """\
-........
-........
-........
-........
-........
-.*......
-........
-........
+4 2 3 1
+RUDL
+-1 -1
+1 0
+
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-yoko = ['a','b','c','d','e','f','g','h','i']
-for i in range(8):
-    S = input()
-    if '*' in S:
-        print (str(yoko[S.find('*')])+str(8-i))
+N,M,H,K = map(int,input().split())
+S = input()
+X,Y = 0,0
+xy = []
+for i in range(M):
+    x,y = map(int,input().split())
+    xy.append([x,y])
+xy = set(map(tuple, xy))
+#print (xy)
+
+for i in range(N):
+    if H-1>=0:
+        H-=1
+    else:
+        print ('No')
+        quit()
+    if S[i]=='R':
+        X+=1
+    elif S[i]=='L':
+        X-=1
+    elif S[i]=='U':
+        Y+=1
+    elif S[i]=='D':
+        Y-=1
+
+    if H<K:
+        if(X,Y)in xy:
+            H=K
+            xy.remove((X,Y))
+
+#xy = list(set(xy))
+#print (xy)
+
+print ('Yes')
