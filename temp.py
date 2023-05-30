9dedc601d8fb4675cff3f60506b0bc0167a18ab7
cahein <incidents1371@gmail.com>
2023-05-30 11:45:59 +0900

    20230530

--- temp.py
+++ temp.py
@@ -3,48 +3,22 @@ import io
 import sys
 
 _INPUT = """\
-4 2 3 1
-RUDL
--1 -1
-1 0
-
+6 5
+3 1 4 1 5 9
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,M,H,K = map(int,input().split())
-S = input()
-X,Y = 0,0
-xy = []
-for i in range(M):
-    x,y = map(int,input().split())
-    xy.append([x,y])
-xy = set(map(tuple, xy))
-#print (xy)
+N,X = map(int,input().split())
+Alist = list(map(int,input().split()))
+A=set(Alist)
 
-for i in range(N):
-    if H-1>=0:
-        H-=1
-    else:
-        print ('No')
-        quit()
-    if S[i]=='R':
-        X+=1
-    elif S[i]=='L':
-        X-=1
-    elif S[i]=='U':
-        Y+=1
-    elif S[i]=='D':
-        Y-=1
+for i in A:
+    if i - X in A:
+        print ('Yes')
+        exit()
 
-    if H<K:
-        if(X,Y)in xy:
-            H=K
-            xy.remove((X,Y))
 
-#xy = list(set(xy))
-#print (xy)
-
-print ('Yes')
+print ('No')
