2a280adc60880050b5506a09d26e2ca9030a92e0
cahein <incidents1371@gmail.com>
2023-05-30 15:13:59 +0900

    20230530

--- temp.py
+++ temp.py
@@ -3,22 +3,19 @@ import io
 import sys
 
 _INPUT = """\
-6 5
-3 1 4 1 5 9
+103
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,X = map(int,input().split())
-Alist = list(map(int,input().split()))
-A=set(Alist)
+X = int(input())
 
-for i in A:
-    if i - X in A:
-        print ('Yes')
-        exit()
-
-
-print ('No')
+result= 100
+n=0
+while(result<X):
+    result =  result+result//100
+    #print (result)
+    n+=1
+print (n)
