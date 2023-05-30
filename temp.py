c5afe93ae9b10bf2b750fc98fd1939767c2b39f5
cahein <incidents1371@gmail.com>
2023-05-30 15:51:51 +0900

    20230530

--- temp.py
+++ temp.py
@@ -3,19 +3,15 @@ import io
 import sys
 
 _INPUT = """\
-103
-
+11 10 9
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-X = int(input())
+A,B,N = map(int,input().split())
 
-result= 100
-n=0
-while(result<X):
-    result =  result+result//100
-    #print (result)
-    n+=1
-print (n)
+ans = (A*N//B)-A*(N//B)
+if B-1<=N:
+    ans = max(ans,(A*(B-1)//B)-A*((B-1)//B))
+print (ans)
