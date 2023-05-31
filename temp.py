fa0b48c88d03547573f4ea5e812b6f30fa1665d8
cahein <incidents1371@gmail.com>
2023-05-31 16:42:12 +0900

    20230531

--- temp.py
+++ temp.py
@@ -3,21 +3,23 @@ import io
 import sys
 
 _INPUT = """\
-5 15
+7
+50
+30
+50
+100
+50
+80
+30
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-K,S = map(int,input().split())
-
-z = 0
-ans = 0
-for i in range(K+1):
-    for j in range(K+1):
-        z = S-i-j
-        if i+j+z==S and z>=0 and z<=K:
-            ans+=1
-            #print (i,j,z)
-print (ans)
-
+N = int(input())
+D = []
+for i in range(N):
+    d = int(input())
+    D.append(d)
+D = set(D)
+print (len(D))
