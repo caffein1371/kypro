f581148451eca49d72514917a77063322fd5aa4f
cahein <incidents1371@gmail.com>
2023-05-31 15:44:41 +0900

    20230531

--- temp.py
+++ temp.py
@@ -3,15 +3,19 @@ import io
 import sys
 
 _INPUT = """\
-11 10 9
+2000 20000000
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A,B,N = map(int,input().split())
+N,Y = map(int,input().split())
 
-ans = (A*N//B)-A*(N//B)
-if B-1<=N:
-    ans = max(ans,(A*(B-1)//B)-A*((B-1)//B))
-print (ans)
+c = 0
+for i in range(N+1):
+    for j in range(N+1):
+        c = N-i-j
+        if 10000*i+5000*j+1000*c ==Y and c>=0:
+            print (i,j,c)
+            exit()
+print (-1,-1,-1)
