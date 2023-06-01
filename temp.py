662c160c8caead93d908c6bcbc66e7644cdecd7d
cahein <incidents1371@gmail.com>
2023-06-01 09:38:27 +0900

    20230601

--- temp.py
+++ temp.py
@@ -3,12 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-5 5
-4 2 3 4 5
-4 1 3 4 5
-4 1 2 4 5
-4 1 2 3 5
-4 1 2 3 4
+5 2
+1 1 2 2 5
+
 
 
 
@@ -16,13 +13,26 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,M = map(int,input().split())
+N,K = map(int,input().split())
+Alist = list(map(int,input().split()))
 
-ans = [i for i in range(1,31)]
-ans = set(ans)
-#print (ans)
-for i in range(N):
-    Kalist = list(map(int,input().split()))
-    alist = set(Kalist[1:])
-    ans = ans & alist
-print (len(ans))
+ans = len(set(Alist))
+if ans<=K:
+    print (0)
+    exit()
+
+from collections import Counter
+
+temp = Counter(Alist)
+
+temp1 = dict(sorted(temp.items()))
+
+
+i=0
+Ans = 0
+while ans>K:
+    
+    ans-=temp1[i].values()
+    Ans+=temp[i].values()
+    i+=1
+print (Ans)
