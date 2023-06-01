7f5b4cb62f40b0b28c4679e48fba25f66141d84e
cahein <incidents1371@gmail.com>
2023-06-01 10:22:46 +0900

    20230601

--- temp.py
+++ temp.py
@@ -3,14 +3,11 @@ import io
 import sys
 
 _INPUT = """\
-7
-beat
-vet
-beet
-bed
-vet
-bet
-beet
+4
+2
+5
+5
+2
 
 
 
@@ -19,21 +16,12 @@ beet
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-S = []
 
+temp = set()
 for i in range(N):
-    s = input()
-    s = ''.join(s)
-    S.append(s)
-from collections import Counter
-temp = Counter(S)
-c = Counter(S)
-values, counts = zip(*c.most_common())
-maxcount = counts[0]
-
-#print (maxans)
-temp = sorted(dict(temp).items())
-#print (temp)
-for i in range(len(temp)):
-    if maxcount==temp[i][1]:
-        print (temp[i][0])
+    A = int(input())
+    if A in temp: 
+        temp.remove(A)
+    else:
+        temp.add(A)
+print (len(temp))
