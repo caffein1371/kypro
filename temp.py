13052c20f04a454279e9fe68651758aaaaba9502
cahein <incidents1371@gmail.com>
2023-06-01 10:15:26 +0900

    20230601

--- temp.py
+++ temp.py
@@ -3,9 +3,14 @@ import io
 import sys
 
 _INPUT = """\
-5 2
-1 1 2 2 5
-
+7
+beat
+vet
+beet
+bed
+vet
+bet
+beet
 
 
 
@@ -13,26 +18,22 @@ _INPUT = """\
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N,K = map(int,input().split())
-Alist = list(map(int,input().split()))
+N = int(input())
+S = []
 
-ans = len(set(Alist))
-if ans<=K:
-    print (0)
-    exit()
-
+for i in range(N):
+    s = input()
+    s = ''.join(s)
+    S.append(s)
 from collections import Counter
+temp = Counter(S)
+c = Counter(S)
+values, counts = zip(*c.most_common())
+maxcount = counts[0]
 
-temp = Counter(Alist)
-
-temp1 = dict(sorted(temp.items()))
-
-
-i=0
-Ans = 0
-while ans>K:
-    
-    ans-=temp1[i].values()
-    Ans+=temp[i].values()
-    i+=1
-print (Ans)
+#print (maxans)
+temp = sorted(dict(temp).items())
+#print (temp)
+for i in range(len(temp)):
+    if maxcount==temp[i][1]:
+        print (temp[i][0])
