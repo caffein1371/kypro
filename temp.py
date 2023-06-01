eb93eaca59eba177ec12ca74207985e620b68eb3
cahein <incidents1371@gmail.com>
2023-06-01 17:36:06 +0900

    20230601

--- temp.py
+++ temp.py
@@ -3,14 +3,12 @@ import io
 import sys
 
 _INPUT = """\
-10
-aabbbbaaca
+aabbbbbbbbbbbbxyza
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
 S = input()
 i = 0
 ans = []
@@ -18,7 +16,7 @@ while i<len(S):
     j=i+1
     while j<len(S) and S[i]==S[j]:
         j+=1
-    ans.append(S[i])
+    ans.append(S[i]+str(j-i))
     i= j
-
-print (len(ans))
+print (''.join(ans))
+#print (len(ans))
