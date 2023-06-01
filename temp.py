84a7cf18e4bf3d2bafc91fbf0e1e01e728c7c1b7
cahein <incidents1371@gmail.com>
2023-06-01 10:40:03 +0900

    20230601

--- temp.py
+++ temp.py
@@ -4,24 +4,28 @@ import sys
 
 _INPUT = """\
 4
-2
-5
-5
-2
-
-
+3 3 3 3
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
+b = list(map(int,input().split()))
 
-temp = set()
-for i in range(N):
-    A = int(input())
-    if A in temp: 
-        temp.remove(A)
-    else:
-        temp.add(A)
-print (len(temp))
+from collections import Counter
+
+temp = Counter(b)
+temp = dict(temp)
+#print (temp)
+temp[0]=0
+ans = 0
+for key,value in temp.items():
+    #print (key)
+    #print (value)
+    if key>value:
+        ans+=value
+    elif key<value:
+        ans+=value-key
+
+print (ans)
