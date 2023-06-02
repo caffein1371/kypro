612fe4910dbe8aa416ddac9fe8f752fb7eb27363
cahein <incidents1371@gmail.com>
2023-06-02 13:09:38 +0900

    20230602

--- temp.py
+++ temp.py
@@ -3,18 +3,39 @@ import io
 import sys
 
 _INPUT = """\
-7
-1 2 3 4 5 6
+4 3
+1 2 3 4
+1 3
+2 3
+2 4
+
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-Alist = list(map(int,input().split()))
-G = [0 for i in range(N)]
-for i in range(len(Alist)):
-    G[Alist[i]-1]+=1
-
+N,M = map(int,input().split())
+Hlist = list(map(int,input().split()))
+G = [[]for i in range(N)]
+for i in range(M):
+    a,b = map(int,input().split())
+    a-=1
+    b-=1
+    G[a].append(b)
+    G[b].append(a)
+ans = 0
 for i in range(N):
-    print (G[i])
+    # print (G[i])
+    maxhigh=0
+    if len(G[i])>0:
+        for v in G[i]:
+            maxhigh = max(Hlist[v],maxhigh)
+        if maxhigh<Hlist[i]:
+            ans+=1
+    #     for v in G[i]:
+    #         print (v)
+    #         # if G[v]>=max(v):
+    #             # ans+=1
+    else:
+        ans+=1
+print (ans)
