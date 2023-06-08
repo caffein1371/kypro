df8175d4185ec3e9c49bbbe2b2c5b58da3cd24b8
cahein <incidents1371@gmail.com>
2023-06-09 08:54:31 +0900

    20230608

--- temp.py
+++ temp.py
@@ -3,40 +3,18 @@ import io
 import sys
 
 _INPUT = """\
-3
-3
-1
-2
-
-
+5 3 4
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
-ans = []
-for i in range(N):
-    a = int(input())
-    a-=1
-    ans.append(a)
-tansaku = set()
-temp = 0
-i = 1
-while True:
-    if ans[temp]==1:
-        print (i)
-        break
-    if ans[temp] not in tansaku:
-        tansaku.add(ans[temp])
-        #tansaku = liset(tansaku))
+A,B,N = map(int,input().split())
 
-    else:
-        print (-1)
-        break
-    temp = ans[temp]
-    i+=1
+ans = (A*N//B)-A*(N//B)
+if B-1<=N:
+    ans = (A*(B-1)//B)-A*((B-1)//B)
 
 
-    
+print (ans)
