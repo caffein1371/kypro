4893bf35f08a177aadffb08ad0652a1b07699534
cahein <incidents1371@gmail.com>
2023-06-08 16:04:17 +0900

    20230608

--- temp.py
+++ temp.py
@@ -3,24 +3,21 @@ import io
 import sys
 
 _INPUT = """\
-5
-10 20 30 40 50
+6 3
+2 0 2 -1 0 -4
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-N = int(input())
+N,K = map(int,input().split())
 Alist = list(map(int,input().split()))
 temp = [0 for i in range(N+1)]
 for i in range(N):
     temp[i+1]=Alist[i]+temp[i]
 #print (temp)
 
-for i in range(1,N+1):
-    ans = 0
-    for j in range(N-i+1):
-        diff = temp[j+i]-temp[j]
-        ans = max(diff,ans)
-    print (ans)
+for i in range(N-K+1):
+    diff = temp[i+K]-temp[i]
+    print (diff)
 
