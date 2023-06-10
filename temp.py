fd0217a82dd6737c206a2373b22f7f4d8e60c65e
cahein <incidents1371@gmail.com>
2023-06-10 09:33:13 +0900

    20230610

--- temp.py
+++ temp.py
@@ -3,18 +3,34 @@ import io
 import sys
 
 _INPUT = """\
-5 3 4
+2
+3 1 2
+6 1 1
 
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
-A,B,N = map(int,input().split())
+N = int(input())
 
-ans = (A*N//B)-A*(N//B)
-if B-1<=N:
-    ans = (A*(B-1)//B)-A*((B-1)//B)
-
-
-print (ans)
+tempt,tempx,tempy = 0,0,0
+for i in range(N):
+    t,x,y = map(int,input().split())
+    jikan = abs(t-tempt)
+    xsa = abs(x-tempx)
+    ysa = abs(y-tempy)
+    if jikan<(xsa+ysa):
+        #flag = False
+        print ('No')
+        exit()
+    elif jikan%2==1 and (xsa+ysa)%2==0:
+        print ('No')
+        exit()
+        #flag = False
+    elif jikan%2==0 and (xsa+ysa)%2==1:
+        print ('No')
+        exit()
+        # flag = False
+    tempt,tempx,tempy=t,x,y
+print ('Yes')
