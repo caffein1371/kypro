eaa999a20613ae144dd18bfeb5b96127b81c2906
cahein <incidents1371@gmail.com>
2023-06-18 17:58:18 +0900

    20230618

--- temp.py
+++ temp.py
@@ -3,56 +3,52 @@ import io
 import sys
 
 _INPUT = """\
-21
-0 20 62 192 284 310 323 324 352 374 409 452 486 512 523 594 677 814 838 946 1000
-10
-77 721
-255 541
-478 970
-369 466
-343 541
-42 165
-16 618
-222 592
-730 983
-338 747
+15
+1 900000000
+0 600000000
+1 -300000000
+0 -700000000
+1 200000000
+1 300000000
+0 -600000000
+1 -900000000
+1 600000000
+1 -100000000
+1 -400000000
+0 900000000
+0 200000000
+1 -500000000
+1 900000000
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-Alist = list(map(int,input().split()))
-Q = int(input())
-temp = [0 for i in range(N+1)]
-for i in range(N-1):
-    if i%2!=0:
-        temp[i+1]=Alist[i+1]-Alist[i]
+
+DP = [[-float('INF')]*2 for i in range(N+1)]
+DP[0][0]=0
+#print (DP)
+x = []
+y = []
 for i in range(N):
-    temp[i+1]+=temp[i]
-# print (temp)
-# print (Alist)
-import bisect
+    X,Y = map(int,input().split())
+    x.append(X)
+    y.append(Y)
 
-def cal(num):
-    ans0 = bisect.bisect_right(Alist,num)
-    ans0-=1
-    #print (ans0)
-    answer = 0
-    if ans0<0:
-        return answer
+for i in range(N):
+    #何も食べない時
+    DP[i+1][0] = max(DP[i+1][0],DP[i][0])
+    DP[i+1][1] = max(DP[i+1][1],DP[i][1])
 
-    if ans0%2==1:
-        answer =num-Alist[ans0]+temp[ans0]
-        return answer
+    #解毒剤を飲む時
+    if x[i]==0:
+        #毒を飲んでない状態から解毒
+        DP[i+1][0]=max(DP[i+1][0],DP[i][0]+y[i])
+        #毒を飲んでいる状態から解毒
+        DP[i+1][0]=max(DP[i+1][0],DP[i][1]+y[i])
     else:
-        answer = temp[ans0]
-        return  answer
-        # return temp[ans0]
+        #毒を飲んでいない状態から解毒
+        DP[i+1][1]=max(DP[i][0]+y[i],DP[i+1][1])
 
-
-for i in range(Q):
-    l,r = map(int,input().split())
-    # cal(r)
-    # cal(l)
-    print (cal(r)-cal(l))
+print (max(DP[N][1],DP[N][0]))
