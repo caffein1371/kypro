3ec9274353b85de966d1c4cd516ec18a0b459559
cahein <incidents1371@gmail.com>
2023-06-25 17:09:46 +0900

    20230625

--- temp.py
+++ temp.py
@@ -3,52 +3,33 @@ import io
 import sys
 
 _INPUT = """\
-15
-1 900000000
-0 600000000
-1 -300000000
-0 -700000000
-1 200000000
-1 300000000
-0 -600000000
-1 -900000000
-1 600000000
-1 -100000000
-1 -400000000
-0 900000000
-0 200000000
-1 -500000000
-1 900000000
+8
+a(b(d))c
+
 
 
 """
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
+S = input()
 
-DP = [[-float('INF')]*2 for i in range(N+1)]
-DP[0][0]=0
-#print (DP)
-x = []
-y = []
-for i in range(N):
-    X,Y = map(int,input().split())
-    x.append(X)
-    y.append(Y)
+A = []
+T = []
+from collections import deque
 
-for i in range(N):
-    #何も食べない時
-    DP[i+1][0] = max(DP[i+1][0],DP[i][0])
-    DP[i+1][1] = max(DP[i+1][1],DP[i][1])
-
-    #解毒剤を飲む時
-    if x[i]==0:
-        #毒を飲んでない状態から解毒
-        DP[i+1][0]=max(DP[i+1][0],DP[i][0]+y[i])
-        #毒を飲んでいる状態から解毒
-        DP[i+1][0]=max(DP[i+1][0],DP[i][1]+y[i])
-    else:
-        #毒を飲んでいない状態から解毒
-        DP[i+1][1]=max(DP[i][0]+y[i],DP[i+1][1])
-
-print (max(DP[N][1],DP[N][0]))
+A = deque()
+T = []
+for i in range(len(S)):
+    if S[i]!=')' and S[i]!='(':
+        T.append(S[i])
+    elif S[i]=='(':
+        T.append(S[i])
+        A.append(i)
+    elif S[i]==')':
+        if len(A)!=0:
+            x = A.pop()
+            T = T[0:x]
+        else:
+            T.append(S[i])
+print (''.join(T))
