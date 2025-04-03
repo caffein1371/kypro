a5dbde109e918a62a883b4109ff32d424ad130fe
cahein <incidents1371@gmail.com>
2025-04-03 22:51:12 +0900

    initial message

---
+++ ABC399/A.py
@@ -0,0 +1,19 @@
+##########################################
+import io
+import sys
+
+_INPUT = """\
+6
+abcarc
+agcahc
+
+
+
+"""
+sys.stdin = io.StringIO(_INPUT)
+##########################################
+N = int(input())
+S = input()
+T = input()
+
+print (S)
