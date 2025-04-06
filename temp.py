00f2f522504399dfe28a95f490f20e47caf3e386
cahein <incidents1371@gmail.com>
2025-04-06 22:02:13 +0900

    20250406

--- temp.py
+++ temp.py
@@ -3,8 +3,9 @@ import io
 import sys
 
 _INPUT = """\
-20
-
+8
+7 3 5 4 2 1 6 8
+3 8 2 5 4 6 7 1
 
 
 
@@ -12,20 +13,12 @@ _INPUT = """\
 sys.stdin = io.StringIO(_INPUT)
 ##########################################
 N = int(input())
-from math import isqrt
-
-# a = 0
-# for i in range(1000):
-#     if 2**i>=10**18:
-#         #aは最大でも60
-#         a = i
-#         break
-# aは最大でも60
-# print (a)
-
-ans = 0
-for i in range(1,61):
-    ans+= (isqrt(N//(2**i))+1)//2
-print (ans)
+import itertools
+Plist = tuple(list(map(int,input().split())))
+Qlist = tuple(list(map(int,input().split())))
+temp = [i for i in range(1,N+1)]
+A = list(itertools.permutations(temp))
 
 
+#print (A)
+print (abs(A.index(Plist)-A.index(Qlist)))
