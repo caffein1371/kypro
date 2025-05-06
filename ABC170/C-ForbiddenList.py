f01f2a023ed5123b2909e9d2c84b9bf4d7f160ef
cahein <incidents1371@gmail.com>
2025-05-06 19:28:33 +0900

    20250506

--- ABC170/C-ForbiddenList.py
+++ ABC170/C-ForbiddenList.py
@@ -3,8 +3,7 @@ import io
 import sys
 
 _INPUT = """\
-6 5
-4 7 10 6 5
+100 0
 
 
 """
@@ -12,3 +11,24 @@ sys.stdin = io.StringIO(_INPUT)
 ##########################################
 X,N = map(int,input().split())
 plist = list(map(int,input().split()))
+
+templist = [i for i in range(102)]
+#print (templist)
+
+for i in range(N):
+  templist.remove(plist[i])
+#print (templist)
+
+if N==0:
+  print (X)
+  quit()
+
+
+diff = 10**7
+ans = 10**7
+
+for i in range(len(templist)):
+  if diff>abs(X-templist[i]):
+    diff = abs(X-templist[i])
+    ans = templist[i]
+print (ans)
